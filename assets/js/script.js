8f92a5aae68b7ee974acd4c7a3d011f446ae525e
Mitchell Armstrong <mra0211@gmail.com>
2022-08-15 11:12:58 -0400

    Finished the edit button and it now works as intended

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,20 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var completeEditTask = function (taskName, taskType, taskId) {
+    // find the matching task list item'
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // set new values 
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
+
 var taskFormHandler = function () {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -16,14 +30,23 @@ var taskFormHandler = function () {
 
     formEl.reset();
 
-    // Package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
+    var isEdit = formEl.hasAttribute("data-task-id");
 
-    // Send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
+    // has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+
+    // no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+
+        createTaskEl(taskDataObj);
+    }
 }
 
 var createTaskEl = function (taskDataObj) {
